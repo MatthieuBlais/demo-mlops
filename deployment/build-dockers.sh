c9fdc941e77266adbb99a062fadb4b4d0884b71e
Matthieu Blais <matthieu.blais1@gmail.com>
2021-02-19 21:25:09 +0800

    debugging env

--- deployment/build-dockers.sh
+++ deployment/build-dockers.sh
@@ -8,6 +8,10 @@ fi
 
 cd components/
 
+echo "Project_ID: $PROJECT_ID"
+echo "BRANCH_NAME: $BRANCH_NAME"
+echo "SHORT_SHA: $SHORT_SHA"
+
 # Looping through all components and build if there is a Dockerfile
 for folder in */; do
   if [ -d "$folder" ]; then
@@ -16,6 +20,7 @@ for folder in */; do
     if [[ -f "Dockerfile" ]]; then
         echo "Building Docker image for component $folder..."
         image_name="${folder%?}"
+        echo "sample gcr.io/$PROJECT_ID/demo-mlops/$image_name:$BRANCH_NAME-$SHORT_SHA"
         docker build -t gcr.io/$PROJECT_ID/demo-mlops/$image_name:$BRANCH_NAME-$SHORT_SHA -t gcr.io/$PROJECT_ID/demo-mlops/$image_name:$BRANCH_NAME-latest .
         docker push gcr.io/$PROJECT_ID/demo-mlops/$image_name
     else
