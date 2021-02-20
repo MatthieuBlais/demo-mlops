57bdd830c5fb1d991a3b648d3716493436855b59
Matthieu Blais <matthieu.blais1@gmail.com>
2021-02-20 17:33:05 +0800

    testing aws deployment

---
+++ deployment/aws/build-dockers.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Checking if the repository follows the structure convention
+if [ ! -d "components" ]; then
+    echo "No components, stopping build"
+    exit 1
+fi
+
+# We save the list of docker images as artifact
+touch _artifacts/images.txt
+
+cd components/
+
+echo "REPO_NAME: $PROJECT_NAME"
+echo "BRANCH_NAME: $BRANCH_NAME"
+
+# Looping through all components and build if there is a Dockerfile
+for folder in */; do
+  if [ -d "$folder" ]; then
+    cd $folder
+    ls
+    if [[ -f "Dockerfile" ]]; then
+        echo "Building Docker image for component $folder..."
+        image_name="${folder%?}"
+        aws ecr describe-repositories --repository-names $PROJECT_NAME/$image_name || aws ecr create-repository --repository-name $PROJECT_NAME/$image_name
+        docker build -t $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME/$image_name:$BRANCH_NAME-$CODEBUILD_BUILD_NUMBER -t $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME/$image_name:$BRANCH_NAME-latest .
+        docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME/$image_name
+        echo "$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$PROJECT_NAME/$image_name:$BRANCH_NAME-$CODEBUILD_BUILD_NUMBER" >> ../../_artifacts/images.txt
+    else
+        echo "No Dockerfile for component $folder. Skipping"
+    fi
+    cd ..
+  fi
+done
---
+++ deployment/aws/run-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Checking if the repository follows the structure convention
+if [ ! -d "components" ]; then
+    echo "No components, stopping build"
+    exit 1
+fi
+
+cd components/
+
+# Looping through all components and execute pytest
+for folder in */; do
+  echo $folder
+  if [ -d "$folder" ]; then
+    echo "Testing component $folder..."
+    cd $folder
+    pipenv install pytest pytest-cov --dev
+    pipenv install -r requirements.txt
+    pipenv run python -m pytest --cov app/
+
+    # Saving the coverage report in the artifact folder
+    pipenv run coverage json --pretty-print -o ../../_artifacts/${folder%?}-coverage.json
+    pipenv run python -m pytest --junitxml=../../_reports/${folder%?}-coverage.xml app
+
+    cd ..
+  fi
+done
