ebca853179e0b1bd3254b9d9122fc68e5432c7a6
Matthieu Blais <matthieu.blais1@gmail.com>
2021-02-20 18:49:40 +0800

    update bash to sh

--- deployment/aws/export-branch.sh
+++ deployment/aws/export-branch.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 # Infering branch name
-export BRANCH_NAME=`git symbolic-ref HEAD --short 2>/dev/null`
-if [ "$BRANCH_NAME" == "" ] ; then
-  BRANCH_NAME=`git branch -a --contains HEAD | sed -n 2p | awk '{ printf $1 }'`
-  export BRANCH_NAME=${BRANCH_NAME#remotes/origin/}
+export BRANCH_NAME="$(git symbolic-ref HEAD --short 2>/dev/null)"
+if [ "$BRANCH_NAME" = "" ] ; then
+  export BRANCH_NAME="$(git rev-parse HEAD | xargs git name-rev | cut -d' ' -f2 | sed 's/remotes\/origin\///g')";
 fi
